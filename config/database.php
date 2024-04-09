d61f7d9f2ccb49e1455a440ecebff1bf84a4c68f
Desarrollador <stevenb.09@hotmail.com>
2024-04-09 16:56:27 -0500

    correccion

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
-	
-	class Conectar{
-		
-		public static function conexion(){
-			
-			$conexion = new mysqli("us-cdbr-east-04.cleardb.com","b868986095efeb","45ced77b","heroku_81166031e3e1941");
-			return $conexion;
-			
-		}
+
+class Conectar
+{
+
+	public static function conexion()
+	{
+
+		$conexion = new mysqli("localhost", "root", "Johan12345*", "timeproyect");
+		return $conexion;
 	}
-?>
+}
--- controllers/Usuarios.php
+++ controllers/Usuarios.php
@@ -1,46 +1,52 @@
 <?php
-    class UsuariosController{
+class UsuariosController
+{
 
-        public function __construct(){
-            require_once "models/UsuariosModel.php";
-        }
+	public function __construct()
+	{
+		require_once "models/UsuariosModel.php";
+	}
 
-        public function index(){
+	public function index()
+	{
 
-            $usuarios = new Usuarios_model();
-            $data["usuarios"] = $usuarios->get_usuarios();
+		$usuarios = new Usuarios_model();
+		$data["usuarios"] = $usuarios->get_usuarios();
 
-            require_once "views/usuarios.php";
-        }
+		require_once "views/usuarios.php";
+	}
 
-		public function indexregistro(){
-			
-			$usuarios = new Usuarios_model();
-			$data["usuarios"] = $usuarios->get_usuarios();
-			
-			require_once "index.html";	
-		}
+	public function indexregistro()
+	{
 
-        public function nuevo(){
-			
-			$data["titulo"] = "Usuarios";
-			require_once "views/usuarios_nuevo.php";
-		}
-		
-		public function guarda()
-		{
-            /* el campo id tiene autoincremento en la base de datos = $id = $_POST['id']; */
-            $name = $_POST['nombre'];
-            $last_name = $_POST['apellido'];
-            $email = $_POST['email'];
-            $user_name = $_POST['usuario'];
-            $password = $_POST['contrasena'];
-			
-			$usuarios = new Usuarios_model();
-			$usuarios->insertar($name,$last_name,$email,$user_name,$password);
-			$this->indexregistro();
-		}
+		$usuarios = new Usuarios_model();
+		$data["usuarios"] = $usuarios->get_usuarios();
 
+		require_once "index.html";
+	}
+
+	public function nuevo()
+	{
+
+		$data["titulo"] = "Usuarios";
+		require_once "views/usuarios_nuevo.php";
+	}
+
+	public function guarda()
+	{
+		/* el campo id tiene autoincremento en la base de datos = $id = $_POST['id']; */
+		$name = $_POST['nombre'];
+		$last_name = $_POST['apellido'];
+		$email = $_POST['email'];
+		$user_name = $_POST['usuario'];
+		$password = $_POST['contrasena'];
+
+		$usuarios = new Usuarios_model();
+		$usuarios->insertar($name, $last_name, $email, $user_name, $password);
+		$this->indexregistro();
+	}
+
+	/*
         public function verifica(){
 
 			$usuario = $_POST['usuario'];
@@ -50,34 +56,35 @@
 			$usuarios = new Usuarios_model();
 			$usuarios->ingresa($usuario,$contrasena);
 		}
-		
-		public function modificar($id){
-			
-			$usuarios = new Usuarios_model();
-			
-			$data["id"] = $id;
-			$data["usuarios"] = $usuarios->get_usuario($id);
-			require_once "views/usuarios_modifica.php";
-		}
-		
-		public function actualizar()
-		{
-			$id = $_POST['id'];
-            $name = $_POST['nombre'];
-            $last_name = $_POST['apellido'];
-            $email = $_POST['email'];
+		*/
 
-			$usuarios = new Usuarios_model();
-			$usuarios->modificar($id,$name,$last_name,$email);
-			$this->index();
-		}
-		
-		public function eliminar($id){
-			
-			$usuarios = new Usuarios_model();
-			$usuarios->eliminar($id);
-			$this->index();
-		}
+	public function modificar($id)
+	{
 
-    }
-?>
+		$usuarios = new Usuarios_model();
+
+		$data["id"] = $id;
+		$data["usuarios"] = $usuarios->get_usuario($id);
+		require_once "views/usuarios_modifica.php";
+	}
+
+	public function actualizar()
+	{
+		$id = $_POST['id'];
+		$name = $_POST['nombre'];
+		$last_name = $_POST['apellido'];
+		$email = $_POST['email'];
+
+		$usuarios = new Usuarios_model();
+		$usuarios->modificar($id, $name, $last_name, $email);
+		$this->index();
+	}
+
+	public function eliminar($id)
+	{
+
+		$usuarios = new Usuarios_model();
+		$usuarios->eliminar($id);
+		$this->index();
+	}
+}
